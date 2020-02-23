621cc9e3773067e5a4863fbcf93117791e5bd0af
cptShpek <tarzansh1397@gmail.com>
2020-02-23 18:11:49 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Welcome to Your ToDo List!</h1>
+    <ListCreating style="border: 5px double black; display: flex; justify-content: space-between;"/>
+  </div>
+</template>
+
+<script>
+import ListCreating from './components/ListCreating.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ListCreating,
+  }
+}
+</script>
+
+<style>
+  h1{
+    text-align: center;
+  };
+</style>
---
+++ src/components/ListCreating.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="creating">
+    <div>
+      <h2>Create your todo list here!</h2>
+      <input type="text" v-model="inputValue"><button @click="createTodo( inputValue )">Create!</button>
+    </div>
+    <div>
+      <h2>Your Todo list!</h2>
+      <ul>
+        <li v-for="(todo, index) in todoList" :key="todo">
+          {{todo}} 
+          <button @click="deleteItem( index )">Delete!</button>
+          <button @click="addDoneTodo( index )">Done!</button>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h2>Your Done List Here!</h2>
+      <ul>
+        <li v-for="done in doneList" :key="done">{{done}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+    name: 'ListCreating',
+
+    data() { return {
+
+        inputValue: '',
+        todoList: [],
+        doneList: [],
+
+      }
+    },
+
+    methods: {
+
+      createTodo: function ( el ){
+
+        this.todoList.push( el );
+        return;
+
+      },
+
+      deleteItem: function ( index ){
+
+        this.todoList.splice( index, 1 );
+        return;
+
+      },
+
+      addDoneTodo: function ( index ) {
+
+        this.doneList.push( this.todoList[index] );
+        this.deleteItem( index );
+        return;
+
+      }
+
+    },
+
+  }
+</script>
+
+<style scoped>
+</style>
